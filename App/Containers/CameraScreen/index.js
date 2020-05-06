eacbb27f63d751279c9375dac9af7c0cdada1b7f
Pierre Lavalley <pierre.lavalley@gmail.com>
2020-05-06 19:06:22 +0200

    Add explanation on camera screen

--- App/Containers/CameraScreen/index.js
+++ App/Containers/CameraScreen/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { StatusBar, Vibration } from 'react-native';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { Platform } from 'react-native';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 import { RNCamera } from 'react-native-camera';
 import DeviceInfo from 'react-native-device-info';
+import { withTranslation } from 'react-i18next';
 
 import ProductsActions from '../../Redux/ProductsRedux';
 import Header from '../../Components/Header';
 import { Metrics } from '../../Themes';
 
-export function CameraScreen({ navigation, getProductData, isLoading }) {
+export function CameraScreen({ t, navigation, getProductData, isLoading }) {
   const handleBarcoreRead = e => {
     const type = RNCamera.Constants.BarCodeType.ean13;
     if (e.type === type) {
@@ -35,11 +38,15 @@ export function CameraScreen({ navigation, getProductData, isLoading }) {
         <Header onPress={() => navigation.goBack()} barStyle="light" />
       </HeaderWrapper>
       {isLoading && <StyledActivityIndicator color="#ffffff" size="large" />}
+      <ExplanationWrapper>
+        <Explanation>{t('camera:explanation')}</Explanation>
+      </ExplanationWrapper>
     </Wrapper>
   );
 }
 
 CameraScreen.propTypes = {
+  t: PropTypes.func,
   navigation: PropTypes.object,
   isLoading: PropTypes.bool,
   getProductData: PropTypes.func,
@@ -53,9 +60,14 @@ const mapDispatchToProps = dispatch => ({
   getProductData: ean13 => dispatch(ProductsActions.getOffData(ean13)),
 });
 
-export default connect(
+const withConnect = connect(
   mapStateToProps,
   mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  withTranslation(),
 )(CameraScreen);
 
 const Wrapper = styled.View`
@@ -73,4 +85,21 @@ const StyledActivityIndicator = styled.ActivityIndicator`
   position: absolute;
   left: ${Metrics.screenWidth / 2 - 15};
   top: ${Metrics.screenHeight / 2 - 15};
+`;
+
+const ExplanationWrapper = styled.View`
+  position: absolute;
+  bottom: 50px;
+  left: 20px;
+  width: ${Metrics.screenWidth - 40}px;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 5px;
+  padding: 10px;
+  padding-bottom: ${Platform.OS === 'ios' ? '6px' : '10px'};
+`;
+
+const Explanation = styled.Text`
+  font-family: 'SofiaProRegular';
+  font-size: 14px;
+  text-align: center;
 `;
